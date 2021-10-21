de680dbd2047f1ce283568c0c763317ca7e00e93
Suseendhiran D <suseendhiran.d@f22labs.com>
2021-10-21 10:31:18 +0530

    same-origin

--- scripts.js
+++ scripts.js
@@ -135,7 +135,7 @@ async function getItems(brandName, prodType) {
       mode: "cors",
       cache: "no-cache",
       //headers: { "Content-Type": "application/json" },
-      referrerPolicy: "origin",
+      referrerPolicy: "same-origin",
     }
   )
     .then((data) => data.json())
