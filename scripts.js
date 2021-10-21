fc1cb49731519a9fdc3e7a504ec7e861cc79e3c2
Suseendhiran D <suseendhiran.d@f22labs.com>
2021-10-21 10:34:38 +0530

    unsafe-url

--- scripts.js
+++ scripts.js
@@ -135,7 +135,7 @@ async function getItems(brandName, prodType) {
       mode: "cors",
       cache: "no-cache",
       //headers: { "Content-Type": "application/json" },
-      referrerPolicy: "strict-origin",
+      referrerPolicy: "unsafe-url",
     }
   )
     .then((data) => data.json())
